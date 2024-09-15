7345615b53242191f9b6416cfde98d994a31f56b
Ouattara Junior <ouattarajunior418@gmail.com>
2024-09-15 23:22:15 +0000

    Vues details pour chauffeurs et véhicules

--- chauffeur/admin.py
+++ chauffeur/admin.py
@@ -1,28 +1,18 @@
+# chauffeur/admin.py
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Chauffeur
 
 @admin.register(Chauffeur)
 class ChauffeurAdmin(admin.ModelAdmin):
-    list_display = ('nom', 'prenom', 'age', 'numero_piece', 'email', 'lieu_de_residence', 'nom_pere', 'nom_mere')
+    list_display = ('nom', 'prenom', 'age', 'numero_piece', 'email', 'lieu_de_residence', 'nom_pere', 'nom_mere', 'view_details_button')
     search_fields = ('nom', 'prenom', 'numero_piece')
     list_filter = ('age', 'lieu_de_residence')
     ordering = ('nom', 'prenom')
 
-    def get_queryset(self, request):
-        qs = super().get_queryset(request)
-        # Inclure des informations supplémentaires si nécessaire
-        return qs
+    def view_details_button(self, obj):
+        url = reverse('chauffeur_details', args=[obj.id])
+        return format_html('<a class="button" href="{}">Voir Détails</a>', url)
 
-    def chauffeur_details(self, obj):
-        # Exemple de méthode pour afficher des détails personnalisés
-        return f"Email: {obj.email} | Téléphone: {obj.telephone}"
-
-    chauffeur_details.short_description = 'Détails Chauffeur'
-
-    def change_view(self, request, object_id, form_url='', extra_context=None):
-        # Ajoutez des informations supplémentaires à la page de détail
-        extra_context = extra_context or {}
-        chauffeur = self.get_object(request, object_id)
-        extra_context['details'] = f"Chauffeur {chauffeur.nom} {chauffeur.prenom} : Email - {chauffeur.email}"
-        return super().change_view(request, object_id, form_url, extra_context=extra_context)
-
+    view_details_button.short_description = 'Voir Détails'
--- vehicule/admin.py
+++ vehicule/admin.py
@@ -1,14 +1,21 @@
+# vehicules/admin.py
 from django.contrib import admin
-from .models import HistoriqueChauffeur, Vehicule
+from django.urls import reverse
+from django.utils.html import format_html
+from .models import Vehicule, HistoriqueChauffeur
 
 @admin.register(Vehicule)
 class VehiculeAdmin(admin.ModelAdmin):
-    list_display = ('marque', 'modele', 'annee', 'couleur', 'immatriculation', 'kilometrage', 'date_achat', 'chauffeur')
+    list_display = ('marque', 'modele', 'annee', 'couleur', 'immatriculation', 'kilometrage', 'date_achat', 'chauffeur', 'view_details_button')
     search_fields = ('marque', 'modele', 'immatriculation', 'chauffeur__nom', 'chauffeur__prenom')
     list_filter = ('annee', 'couleur')
     ordering = ('marque', 'modele')
 
+    def view_details_button(self, obj):
+        url = reverse('vehicule_details', args=[obj.id])
+        return format_html('<a class="button" href="{}">Voir Détails</a>', url)
 
+    view_details_button.short_description = 'Voir Détails'
 
 @admin.register(HistoriqueChauffeur)
 class HistoriqueChauffeurAdmin(admin.ModelAdmin):
